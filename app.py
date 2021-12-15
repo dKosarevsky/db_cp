370418a29b1851caa8e22eb51cb0f480e65519cd
kosarevskiydp <kosarevskiydp@pik.ru>
2021-12-16 00:44:45 +0300

    init app

---
+++ app.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import psycopg2
+
+# st.set_page_config(initial_sidebar_state="collapsed")
+
+
+def header():
+    author = """
+        made by [Kosarevsky Dmitry](https://github.com/dKosarevsky) 
+        for [DB](https://github.com/dKosarevsky/iu7/blob/master/7sem/db.md) course project
+        in [BMSTU](https://bmstu.ru)
+    """
+    st.markdown("# МГТУ им. Баумана. Кафедра ИУ7")
+    st.markdown("## Базы данных")
+    st.markdown("**Преподаватель:** Павлюк А.А.")
+    st.markdown("**Студент:** Косаревский Д.П.")
+    st.sidebar.image('logo.png', width=300)
+    st.sidebar.markdown(author)
+
+
+# Initialize connection.
+# Uses st.cache to only run once.
+# @st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
+def init_connection():
+    return psycopg2.connect(**st.secrets["postgres"])
+    # return psycopg2.connect(st.secrets["postgres"])
+
+
+# Perform query.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
+def run_query(conn, query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+
+def main():
+    header()
+
+    st.markdown(st.secrets["postgres"])
+
+    conn = init_connection()
+
+    # rows = run_query("SELECT * from mytable;")
+
+    # Print results.
+    # for row in rows:
+    #     st.write(f"{row[0]} has a :{row[1]}:")
+
+
+if __name__ == "__main__":
+    main()
+
