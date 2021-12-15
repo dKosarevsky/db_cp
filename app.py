1caf44b5ed74fb41393e1271f5bf7031820e74a6
kosarevskiydp <kosarevskiydp@pik.ru>
2021-12-16 01:09:58 +0300

    test db v4

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def header():
 
 # Initialize connection.
 # Uses st.cache to only run once.
-# @st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
+@st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
 def init_connection():
     return psycopg2.connect(**st.secrets["postgres"])
 
