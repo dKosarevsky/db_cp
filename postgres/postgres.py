22f972b5656ab1ecf94a0ae83a2223c847a6d19f
kosarevskiydp <kosarevskiydp@pik.ru>
2021-12-17 12:30:59 +0300

    upd pg

---
+++ postgres/postgres.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import psycopg2
+
+
+# Initialize connection.
+# Uses st.cache to only run once.
+# @st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
+def init_connection():
+    return psycopg2.connect(**st.secrets["postgres"])
+
+
+# Perform query.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
+def run_query(conn, query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+
+def main():
+    st.subheader("Postgres")
+
+    conn = init_connection()
+    st.write(conn)
+    rows = run_query(conn, "SELECT * from information_schema.sql_sizing;")
+
+    # Print results.
+    for row in rows:
+        st.write(f"{row[0]} has a :{row[1]}:")
+
+
+if __name__ == "__main__":
+    main()
