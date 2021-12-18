8ea2399b4b3709ad366567754d48f46ecb8fda37
kosarevskiydp <kosarevskiydp@pik.ru>
2021-12-18 09:59:56 +0300

    add queries

---
+++ queries/customers_sums.sql
@@ -0,0 +1,10 @@
+-- вывести покупателей и суммы их покупок:
+SELECT
+       c.name as customer_name,
+       SUM(p.price) as total_sales
+FROM db_cp.products p
+LEFT JOIN db_cp.purchases pu ON p.id = pu.product_id
+LEFT JOIN db_cp.customers c ON pu.user_id = c.id
+WHERE date is not null
+GROUP BY c.name
+ORDER BY total_sales desc;
