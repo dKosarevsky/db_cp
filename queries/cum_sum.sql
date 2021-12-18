3f5a92345bca8128856dc8baeea7f50b351d83e3
kosarevskiydp <kosarevskiydp@pik.ru>
2021-12-18 10:43:58 +0300

    add queries

---
+++ queries/cum_sum.sql
@@ -0,0 +1,9 @@
+-- найти кумулятивную сумму
+SELECT
+	date,
+	price,
+	SUM(price) OVER (ORDER BY date) as total
+FROM db_cp.products p
+LEFT JOIN db_cp.purchases pu ON p.id = pu.product_id
+WHERE date is not null
+ORDER BY date;
---
+++ queries/sums_by_months.sql
@@ -0,0 +1,10 @@
+-- найти суммы покупок помесячно:
+SELECT
+       SUM(price) as total_sales,
+       EXTRACT(month from date) as month
+FROM db_cp.products p
+LEFT JOIN db_cp.purchases pu ON p.id = pu.product_id
+LEFT JOIN db_cp.customers c ON pu.user_id = c.id
+WHERE date is not null
+GROUP BY EXTRACT(month from date)
+ORDER BY EXTRACT(month from date);
