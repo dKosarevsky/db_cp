66e9e8a9fbf4311fa1eb3dda3c89ac71ff6d58b6
kosarevskiydp <kosarevskiydp@pik.ru>
2022-01-08 21:36:56 +0300

    upd default query

--- postgres/postgres.py
+++ postgres/postgres.py
@@ -115,7 +115,7 @@ def main():
         st.write(age_count_filter)
 
     elif select == 12:
-        query = st.text_area("Введите произвольный запрос:", value="select * from db_cp.transactions limit 10")
+        query = st.text_area("Введите произвольный запрос:", value="SELECT version();")
         avg_salary_filter = pd.read_sql(query, conn)
         st.write(avg_salary_filter)
 
